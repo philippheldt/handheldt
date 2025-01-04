21fcb076e1843d1961199893a7301da4432d085a
Philipp Heldt <42078113+philippheldt@users.noreply.github.com>
2025-01-04 11:37:30 +0100

    autoplay

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,15 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: false,
+    loop: true,
     effect: 'cards',
+    speed: 700,
+
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: true,
+        pauseOnMouseEnter: true,
+    },
 
     cardsEffect: {
         perSlideOffset: 6, // Space between cards in px
@@ -26,4 +33,15 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
         hide: 'true',
     },
+})
+
+// Select all videos in the Swiper slides
+const videos = document.querySelectorAll('.blog-video')
+
+// Listen for play events on each video
+videos.forEach((video) => {
+    video.addEventListener('play', () => {
+        // Stop Swiper autoplay when a video starts playing
+        swiper.autoplay.stop()
+    })
 })
